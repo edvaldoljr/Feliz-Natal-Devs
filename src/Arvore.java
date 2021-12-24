7e7581c8db0bf98a2daf2565f2bcce4e6d01cc70
edvaldoljr <edvaldoluizj@gmail.com>
2021-12-24 14:19:03 -0300

    Feliz Natal com Laço for

--- src/Arvore.java
+++ src/Arvore.java
@@ -24,4 +24,34 @@ public class Arvore {
         System.out.println ("    ****    ***********       ****");
         System.out.println ("    ****    ***********       ****");
     }
+
+    public static void obterMensagem2(){
+        int asteristico = 1;
+        int pos = 17;
+
+        for (int i = 0; i < 17; i++) {
+            for (int j = 1; j <= 32; j++) {
+                if (j == pos) {
+                    for (int m = 1; m <= asteristico; m++) {
+                        System.out.print("*");
+                    }
+                    asteristico = asteristico + 2;
+                    pos--;
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        for (int x = 1; x <= 2; x++) {
+            System.out.println("          ***********");
+        }
+
+        for (int y = 1; y <= 2; y++) {
+            System.out.println("  ****    ***********       ****");
+        }
+
+        System.out.println("__________________________________");
+        System.out.println("          FELIZ NATAL           ");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
+        //Árvore contruida apenas com Println;
         Arvore.obterMensagem();
+        System.out.println();
+        //Árvore contruida com laço fo();
+        Arvore.obterMensagem2();
     }
 }
 
