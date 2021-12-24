5af6688b55ec5531068608673fcf25b4b43c18dd
edvaldoljr <edvaldoluizj@gmail.com>
2021-12-24 13:07:06 -0300

    Feliz Natal!

---
+++ src/Arvore.java
@@ -0,0 +1,27 @@
+public class Arvore {
+
+    public static void obterMensagem(){
+
+        System.out.println ("                 *");
+        System.out.println ("                ** F");
+        System.out.println ("               **** E");
+        System.out.println ("              ****** L");
+        System.out.println ("             ******** I");
+        System.out.println ("            ********** Z");
+        System.out.println ("           ************ *");
+        System.out.println ("          ************** N");
+        System.out.println ("         **************** A");
+        System.out.println ("        ****************** T");
+        System.out.println ("       ******************** A");
+        System.out.println ("      ********************** L");
+        System.out.println ("     ************************ *");
+        System.out.println ("    ************************** D");
+        System.out.println ("   **************************** E");
+        System.out.println ("  ****************************** V");
+        System.out.println (" ******************************** S");
+        System.out.println ("            ***********"           );
+        System.out.println ("            ***********"           );
+        System.out.println ("    ****    ***********       ****");
+        System.out.println ("    ****    ***********       ****");
+    }
+}
